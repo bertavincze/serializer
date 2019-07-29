eb8ef645fa7c88aff2fa23cb6c70880a0c406627
Berta Vincze <berta.vincze@gmail.com>
2019-07-29 11:27:05 +0200

    Add project files

---
+++ Serializer/Serializer/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializer
+{
+    [Serializable]
+    class Person : ISerializable, IDeserializationCallback
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateRecorded { get; set; }
+
+        [NonSerialized]
+        public int serialNum;
+
+        public Person(string name, string address, string phoneNumber, int serialNum)
+        {
+            Name = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            DateRecorded = DateTime.Now;
+            this.serialNum = serialNum;
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Address = (string)info.GetValue("Address", typeof(string));
+            PhoneNumber = (string)info.GetValue("Phone Number", typeof(string));
+            DateRecorded = (DateTime)info.GetValue("Date Recorded", typeof(DateTime));
+        }
+
+        public void Serialize()
+        {
+            Stream stream = File.Open(@"C:\Users\Tulajdonos\Desktop\Codecool\Serializer\person" + serialNum + ".dat", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public Person DeSerialize(int serialNum)
+        {
+            Stream stream = new FileStream(@"C:\Users\Tulajdonos\Desktop\Codecool\Serializer\person" + serialNum + ".dat", FileMode.Open, FileAccess.Read);
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (Person)formatter.Deserialize(stream);
+        }
+
+        public Person DeSerialize()
+        {
+            string[] directories = Directory.GetFiles(@"C:\Users\Tulajdonos\Desktop\Codecool\Serializer");
+            int serialNum = directories.Length;
+            Stream stream = new FileStream(@"C:\Users\Tulajdonos\Desktop\Codecool\Serializer\person" + serialNum + ".dat", FileMode.Open, FileAccess.Read);
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (Person)formatter.Deserialize(stream);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Address", Address);
+            info.AddValue("Phone Number", PhoneNumber);
+            info.AddValue("Date Recorded", DateRecorded);
+        }
+
+        public void OnDeserialization(object sender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Serializer/Serializer/Serializer.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Serializer
+{
+    partial class Serializer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(22, 25);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(25, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(399, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(25, 94);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(399, 20);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(22, 77);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 2;
+            this.labelAddress.Text = "Address";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(25, 147);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(399, 20);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(22, 130);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(38, 13);
+            this.labelPhone.TabIndex = 4;
+            this.labelPhone.Text = "Phone";
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(25, 187);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 6;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(106, 187);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(349, 187);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(187, 187);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 9;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(268, 187);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 10;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            // 
+            // Serializer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 236);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labelName);
+            this.Name = "Serializer";
+            this.Text = "Serializer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
